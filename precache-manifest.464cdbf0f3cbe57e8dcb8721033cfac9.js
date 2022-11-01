86e00ecadd623a7c4f9b6fcbda3aadc58d62cd69
drkevinfriday <kevinfriday87@gmail,com>
2022-11-01 18:20:52 -0500

    Updates

---
+++ precache-manifest.464cdbf0f3cbe57e8dcb8721033cfac9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9753a7534e2b0538acb7",
+    "url": "/react-gh-pages/static/css/main.c3fbadcb.chunk.css"
+  },
+  {
+    "revision": "9753a7534e2b0538acb7",
+    "url": "/react-gh-pages/static/js/main.9753a753.chunk.js"
+  },
+  {
+    "revision": "c394874c23d43b636260",
+    "url": "/react-gh-pages/static/js/1.c394874c.chunk.js"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "5033fbcafcd6b4c4cf3e5bf4a83f8e54",
+    "url": "/react-gh-pages/index.html"
+  }
+];
