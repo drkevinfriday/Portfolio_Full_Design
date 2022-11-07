327a05cc8002abbfe50ebef42a0cc412d6c102e7
drkevinfriday <kevinfriday87@gmail,com>
2022-11-07 09:29:25 -0600

    test

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import About from './components/about'
 
 function App() {
   return (
-    <main>
+    <>
     <Nav/>
     <Routes>
       <Route exact path="/home" element={<Home />} />
@@ -25,7 +25,7 @@ function App() {
       
     </Routes>
     <Footer />
-    </main>
+    </>
   );
 }
 
