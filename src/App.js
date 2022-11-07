5e80a77dad2c44f0b0b42d82294c97e9763581a2
drkevinfriday <kevinfriday87@gmail,com>
2022-11-07 12:52:28 -0600

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import React from 'react';
-
-import {  Routes, Route } from "react-router-dom";
+import React from "react";
+import {
+  Router,
+  Routes,
+  Route,
+  Link,
+  BrowserRouter
+} from "react-router-dom";
 
 import Nav from './components/nav'
 // import Header from './components/header'
@@ -14,18 +19,20 @@ import About from './components/about'
 
 function App() {
   return (
-    <main>
+    <>
     <Nav />
-    <Routes>
-      <Route exact path="/" element={<Home />} />
-      <Route path="/contact" element={<Contact />} />
-     <Route path="/about" element={<About />} />
-      <Route path="/work" element={<Work />} />
-      
-      
-    </Routes>
+   
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/work" element={<Work />} />
+        
+        
+      </Routes>
+   
     <Footer />
-    </main>
+    </>
   );
 }
 
